41d63165b51075be8a3743f40d92968de8fba64f
woniu579 <160648442+woniu579@users.noreply.github.com>
2024-04-02 11:01:37 +0800

    Site updated: 2024-04-02 11:01:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/06/12/网络安全01/"];function toRandomPost(){
+var posts=["2024/03/29/网络安全01/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
