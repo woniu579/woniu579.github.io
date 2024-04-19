b59dc9229c4ed72f10b22cdc289991a3419899bc
woniu579 <160648442+woniu579@users.noreply.github.com>
2024-04-19 11:27:12 +0800

    Site updated: 2024-04-19 11:26:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=[];function toRandomPost(){
+var posts=["2024/04/18/“Work-hard-for-yourself”/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
