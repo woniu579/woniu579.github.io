77fbb4354fa997885d89fb742c922fefc042aa29
woniu579 <160648442+woniu579@users.noreply.github.com>
2024-04-23 18:50:44 +0800

    Site updated: 2024-04-23 18:50:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/06/07/风很温柔，花很烂漫，你很特别，我很喜欢！/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
