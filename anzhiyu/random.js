b28c5a64ed24f54de89b207cb4edafb11a76b807
woniu579 <160648442+woniu579@users.noreply.github.com>
2024-04-23 11:18:10 +0800

    Site updated: 2024-04-23 11:17:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/18/“Work-hard-for-yourself”/"];function toRandomPost(){
+var posts=["2023/05/22/疾风亦有归途！/","2023/06/07/风很温柔，花很烂漫，你很特别，我很喜欢！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
