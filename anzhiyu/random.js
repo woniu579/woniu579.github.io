a0108e11c31139fdd1f4ba8c1f1ef20793633b2b
woniu579 <160648442+woniu579@users.noreply.github.com>
2024-08-16 23:11:09 +0800

    Site updated: 2024-08-16 23:10:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/10/Study Goals/","2023/05/22/疾风亦有归途！/","2023/05/04/大提琴Cello笔记/"];function toRandomPost(){
+var posts=["2023/05/10/Study Goals/","2023/05/04/大提琴Cello要点/","2023/05/22/疾风亦有归途！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
